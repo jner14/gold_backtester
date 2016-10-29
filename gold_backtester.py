3f268e7136c624135acb5a37e24450d2108e0422
Jason Robinson <r.jason.robinson12@gmail.com>
2016-10-29 15:47:02 -0700

    Runs clear through, but rebalancing seems to not be working.

--- gold_backtester.py
+++ gold_backtester.py
@@ -9,12 +9,23 @@ import matplotlib.pyplot as plt
 PICKS_CSV_PATH  = 'symbols/gold_picks.csv'
 GDX_CSV_PATH    = 'symbols/gold_gdx.csv'
 DB_FILEPATH     = 'data/daily_gold.db'
-SIGNALS_PATH    = 'csv/signal_data.csv'
+SIGNALS_PATH    = 'signals/signal_data.csv'
+OUTPUT_PATH     = 'output/'
 
 # Parameters
 REBAL_PERIOD    = 1                         # Number of months between rebalance
 START_BALANCE   = 100000.                   # Starting cash balance in portfolio
-START_DAY       = '2007_01_31'              # Day of initial stock purchases
+START_DAY       = '2016_01_04'              # Day of initial stock purchases
+COMMISSION      = .005                      # Cost in dollars per share traded
+COMMISSION_MIN  = 1.                        # Minimum cost in dollars per stock traded
+COMMISSION_MAX  = .005                      # Maximum cost in percent of trade value
+SLIPPAGE        = .01                       # Average slippage in price due to market volatility
+# TEMP LINES BELOW
+COMMISSION      = .0                        # Cost in dollars per share traded
+COMMISSION_MIN  = .0                        # Minimum cost in dollars per stock traded
+COMMISSION_MAX  = .0                        # Maximum cost in percent of trade value
+SLIPPAGE        = .0                        # Average slippage in price due to market volatility
+
 
 # Create QuoteManager object
 quote_manager = QuoteManager(DB_FILEPATH)
@@ -23,45 +34,40 @@ quote_manager = QuoteManager(DB_FILEPATH)
 my_account = AccountManager(START_BALANCE, quote_manager)
 
 # Create OrderManager object
-order_manager = OrderManager(quote_manager, my_account)
+order_manager = OrderManager(quote_manager, 
+                             my_account,
+                             slippage       = SLIPPAGE,
+                             commission_min = COMMISSION_MIN,
+                             commission     = COMMISSION,
+                             commission_max = COMMISSION_MAX
+                             )
+
+# Create an order_history DataFrame
+#order_history = pd.DataFrame(columns=['qty', 'price', 'cost'])
+order_history = {}
 
 # Load signals data
 signals = pd.read_csv(SIGNALS_PATH, index_col=0)
-whats_left = [date.replace('-', '_') for date in signals.index]
-whats_done = []
+signal_dates = [date.replace('-', '_') for date in signals.index]
 
 # Load GDX component symbols
 gdx_symbols = pd.read_csv(GDX_CSV_PATH).symbol
 gdx_symbols = gdx_symbols[gdx_symbols.isin(signals.columns)]
 
 # Skip days until START_DAY is found
-while whats_left[0] != START_DAY: whats_left.pop(0)
-whats_now = whats_left.pop(0)
+while signal_dates[0] != START_DAY: signal_dates.pop(0)
 
-# Get undervalued stock for initial long positions
-undervalued_stock = get_undervalued(signals, whats_now, quote_manager)
-
-# Get top GDX stock for initial short positions, excluding undervalued_stock
-top_gdx_stock = get_top_gdx(gdx_symbols, quote_manager, undervalued_stock)
-
-# Get five percent of current portfolio value
-dollar_amount = my_account.get_percent_account_value(.05)
-
-# Execute initial buy orders
-for stock in undervalued_stock.index:
-    confirmation = order_manager.buy(dollar_amount, stock, whats_now)
-    print('Bought %s because it is now on the undervalued list' % stock)
-
-# Execute initial short orders
-for stock in top_gdx_stock: 
-    confirmation = order_manager.short(dollar_amount, stock, whats_now)
-    print('Shorted %s because it is now on the gdx list' % stock)
+# Create variables to store data from the backtest to be saved in output folder
+index = ['Portfolio Value', 'Cash'] + \
+        ['Long Position {}'.format(i+1) for i in range(10)] + \
+        ['Short Position {}'.format(i+1) for i in range(10)] + \
+        ['Trade {}'.format(i+1) for i in range(40)] 
+history = pd.DataFrame(index=index)
 
 # Get month close rebalance days determined by REBAL_PERIOD
-rebalance_days = get_rebal_days(whats_left, REBAL_PERIOD)
+rebalance_days = get_rebal_days(signal_dates, REBAL_PERIOD)
 
 # Perform rebalancing every REBAL_PERIOD of months
-history = pd.DataFrame(columns=['Account Value', 'Cash'])
 for date in rebalance_days:
 
     print(" "*85 + date)
@@ -80,7 +86,7 @@ for date in rebalance_days:
     long_positions = my_account.get_long_positions()
     for stock in long_positions.index:
         if stock not in new_undervalued.index:
-            confirmation = order_manager.sell_all(stock, date)
+            order_history[stock] = order_manager.sell_all(stock, date)
             print('Sold %s because it is no longer on undervalued list' % stock)
             
     # Sell portion of stock on undervalued list that exceeds 5% of account value
@@ -90,9 +96,9 @@ for date in rebalance_days:
         five_percent_account = .05 * account_value
         current_price = quote_manager.get_quote(stock, date)
         value = current_price * abs(long_positions.qty[stock])
-        if value > five_percent_account:
-            diff_value = value - five_percent_account
-            confirmation = order_manager.sell(diff_value + current_price, stock, date)
+        diff_value = value - five_percent_account
+        if diff_value > current_price:
+            order_history[stock] = order_manager.sell(diff_value + current_price, stock, date)
             print('Sold some of %s because its value exceeds 5%% of portfolio' % stock)
             new_comp = 100.0 * my_account.get_value_stock(stock) / account_value
             print('New % of portfolio for {}: {:.3}'.format(stock, new_comp))
@@ -101,10 +107,10 @@ for date in rebalance_days:
     short_positions = my_account.get_short_positions()
     for stock in short_positions.index:
         if stock in new_undervalued.index:
-            confirmation = order_manager.cover_all(stock, date, )
+            order_history[stock] = order_manager.cover_all(stock, date, )
             print('Covered %s because it is now on undervalued list' % stock)
         elif stock not in new_top_gdx:
-            confirmation = order_manager.cover_all(stock, date)
+            order_history[stock] = order_manager.cover_all(stock, date)
             print('Covered %s because it is no longer on gdx list' % stock)
 
     # Cover portion of stock on gdx list that exceeds 5% of account value
@@ -114,9 +120,9 @@ for date in rebalance_days:
         five_percent_account = .05 * account_value
         current_price = quote_manager.get_quote(stock, date)
         value = current_price * abs(short_positions.qty[stock])
-        if value > five_percent_account:
-            diff_value = value - five_percent_account
-            confirmation = order_manager.cover(diff_value + current_price, stock, date)
+        diff_value = value - five_percent_account
+        if diff_value > current_price:
+            order_history[stock] = order_manager.cover(diff_value + current_price, stock, date)
             print('Covered some of %s because its value exceeds 5%% of portfolio' % stock)
             new_comp = 100.0 * my_account.get_value_stock(stock) / account_value
             print('New % of portfolio for {}: {:.3}'.format(stock, new_comp))
@@ -127,7 +133,7 @@ for date in rebalance_days:
         account_value = my_account.get_account_value(date)
         five_percent_account = .05 * account_value
         if stock not in long_positions.index:
-            confirmation = order_manager.buy(five_percent_account, stock, date)
+            order_history[stock] = order_manager.buy(five_percent_account, stock, date)
             print('Bought %s because it is now on the undervalued list' % stock)
 
     # Buy more of stock on undervalue list that is below 5% of account value
@@ -137,9 +143,9 @@ for date in rebalance_days:
         five_percent_account = .05 * account_value
         current_price = quote_manager.get_quote(stock, date)
         value = current_price * abs(long_positions.qty[stock])
-        if value < five_percent_account:
-            diff_value = five_percent_account - value
-            confirmation = order_manager.buy(diff_value, stock, date)
+        diff_value = five_percent_account - value
+        if diff_value > current_price:
+            order_history[stock] = order_manager.buy(diff_value, stock, date)
             print('Bought some more of %s because its value falls below 5%% of portfolio' % stock)
             new_comp = 100.0 * my_account.get_value_stock(stock) / account_value
             print('New % of portfolio for {}: {:.3}'.format(stock, new_comp))
@@ -150,7 +156,7 @@ for date in rebalance_days:
         account_value = my_account.get_account_value(date)
         five_percent_account = .05 * account_value
         if stock not in short_positions.index:
-            confirmation = order_manager.short(five_percent_account, stock, date)
+            order_history[stock] = order_manager.short(five_percent_account, stock, date)
             print('Shorted %s because it is now on the gdx list' % stock)
 
     # Short more of stock on gdx list that is below 5% of account value 
@@ -160,21 +166,38 @@ for date in rebalance_days:
         five_percent_account = .05 * account_value
         current_price = quote_manager.get_quote(stock, date)
         value = current_price * abs(short_positions.qty[stock])
-        if value < five_percent_account:
-            diff_value = five_percent_account - value
-            confirmation = order_manager.short(diff_value, stock, date)
+        diff_value = five_percent_account - value
+        if diff_value > current_price:
+            order_history[stock] = order_manager.short(diff_value, stock, date)
             print('Shorted some more of %s because its value falls below 5%% of portfolio' % stock)
             new_comp = 100.0 * my_account.get_value_stock(stock) / account_value
             print('New % of portfolio for {}: {:.3}'.format(stock, new_comp))
-
+    
+    # Shift variables for next rebalance
     undervalued_stock = new_undervalued
     top_gdx = new_top_gdx
+    
+    # Store transaction and account data from this rebalance
+    long_positions = my_account.get_long_positions().index
+    short_positions = my_account.get_short_positions().index
 
-    history.loc[date] = [account_value, my_account._cash]
+    history[date] = [account_value, my_account._cash] + \
+                    [(stock, my_account.get_value_stock(stock)) for stock in long_positions] + \
+                    [(stock, my_account.get_value_stock(stock)) for stock in short_positions] + \
+                    [(stock, order_results) for stock, order_results in order_history.iteritems()] + \
+                    ["" for _ in range(40-len(order_history))]
+    #my_account.get_positions().qty * [quote_manager.get_quote(stock, date) for stock in my_account.get_positions().index]
+    # END REBALANCE CODE
 
-history.cumsum()
+# Handle stored data by saving files and showing graphs
+order_history_df = pd.DataFrame(order_history)
+
+history.to_csv(OUTPUT_PATH + 'history.csv')
+order_history_df.to_csv(OUTPUT_PATH + 'order_history.csv')
+
+history.loc['Portfolio Value'].cumsum()
 plt.figure()
-history.plot()
+history.loc['Portfolio Value'].plot()
 plt.show()
 
 print "Finished!"
--- order_manager.py
+++ order_manager.py
@@ -4,94 +4,109 @@ class OrderManager(object):
     def __init__(self, 
                  quote_manager,
                  account,
-                 slippage=.05, 
-                 commission_min=1., 
-                 commission=.01
+                 slippage       = .01, 
+                 commission_min = 1., 
+                 commission     = .005,
+                 commission_max = .005
                  ):
         self._quote_manager = quote_manager
-        self.account = account
-        self.slippage = slippage
+        self.account        = account
+        self.slippage       = slippage
         self.commission_min = commission_min
-        self.commission = commission
+        self.commission     = commission
+        self.commission_max = commission_max
         return super(OrderManager, self).__init__()
 
 
     def cover(self, amount, symbol, date):
-        price = self._quote_manager.get_quote(symbol, date)
-        order_qty = int(amount / price)
-        return self._post_order(order_qty, symbol, price, 'cover')
+        return self._post_order(symbol, date, 'cover', order_amt=amount)
 
 
     def cover_all(self, symbol, date):
-        price = self._quote_manager.get_quote(symbol, date)
         order_qty = abs(self.account.get_positions().loc[symbol].qty)
-        return self._post_order(order_qty, symbol, price, 'cover')
+        return self._post_order(symbol, date, 'cover', order_qty=order_qty)
 
 
     def short(self, amount, symbol, date):
-        # TODO: consider checking for counter positions
-        price = self._quote_manager.get_quote(symbol, date)
-        order_qty = int(amount / price)
-        return self._post_order(order_qty, symbol, price, 'short')
+        return self._post_order(symbol, date, 'short', order_amt=amount)
 
 
     def sell(self, amount, symbol, date):
-        price = self._quote_manager.get_quote(symbol, date)
-        order_qty = int(amount / price)
-        return self._post_order(order_qty, symbol, price, 'sell')
+        return self._post_order(symbol, date, 'sell', order_amt=amount)
 
 
     def sell_all(self, symbol, date):
-        price = self._quote_manager.get_quote(symbol, date)
-        order_qty = self.account.get_positions().loc[symbol].qty
-        return self._post_order(order_qty, symbol, price, 'sell')
+        order_qty = abs(self.account.get_positions().loc[symbol].qty)
+        return self._post_order(symbol, date, 'sell', order_qty=order_qty)
 
 
     def buy(self, amount, symbol, date):
-        # TODO: consider checking for counter positions
-        price = self._quote_manager.get_quote(symbol, date)
-        order_qty = int(amount / price)
-        return self._post_order(order_qty, symbol, price, 'long')
+        return self._post_order(symbol, date, 'buy', order_amt=amount)
 
 
-    def _post_order(self, order_qty, symbol, price, order_type):
+    def _post_order(self, symbol, date, order_type, order_qty=None, order_amt=None):
 
         # Determine account changes based on order type
         if order_type == 'cover':
             stock_func = self.account.remove_stock
             cash_func = self.account.deposit_cash
+            fee_multiplier = -1
             multiplier = 1
         elif order_type == 'short':
             stock_func = self.account.add_stock
             cash_func = self.account.withdraw_cash
+            fee_multiplier = -1
             multiplier = -1
         elif order_type == 'sell':
             stock_func = self.account.remove_stock
             cash_func = self.account.deposit_cash
+            fee_multiplier = -1
             multiplier = -1
-        elif order_type == 'long':
+        elif order_type == 'buy':
             stock_func = self.account.add_stock
             cash_func = self.account.withdraw_cash
+            fee_multiplier = 1
             multiplier = 1
         else:
             raise Exception, "Invalid order_type in order_manager._post_order()!!"
+
+        price = self._quote_manager.get_quote(symbol, date) + self.slippage * multiplier
+
+        if order_amt != None:
+            order_qty = int(order_amt / price)
         
         succeeded = False
-
         while not succeeded:
+            shares_value = order_qty * price
             commission_total = max((self.commission * order_qty), self.commission_min)
-            cost = order_qty * (price + self.slippage) + commission_total
-            succeeded = cash_func(cost)
+            if commission_total > 1.:
+                commission_total = min(commission_total, self.commission_max * shares_value)
+                
+            transfer_amt = commission_total * fee_multiplier + shares_value
+            if order_amt != None and transfer_amt > order_amt:
+                order_qty -= 1
+                continue
+                if order_qty <= 0:
+                    transfer_amt = 0
+                    break
+
+            succeeded = cash_func(transfer_amt)
 
             if succeeded:
-                # TODO: consider adding slippage and commission to average price per share
+                # TODO: consider adding commission to average price per share
                 stock_func(symbol, order_qty * multiplier, price)
             else:
                 order_qty -= 1
                 if order_qty <= 0: 
+                    transfer_amt = 0
                     break
 
-        return {'qty': order_qty * multiplier, 'price': price, 'cost': cost}
+        return {'shares'        : order_qty, 
+                'price'         : price, 
+                'transfer_amt'  : transfer_amt,
+                'type'          : order_type,
+                'commission'    : commission_total
+                }
 
 
 # Used for debugging and development
